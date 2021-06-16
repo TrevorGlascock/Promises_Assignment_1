b172043957b3b841ad07cc09a9d5c1b9cf571d4c
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-06-16 15:15:22 -0500

    returning straight away in getFortune and added comments all around

--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
 function getFortune(question) {
-  const result = tell(question)
+  return tell(question)
     .then((fortune) => [
       `Your question was: ${question}`,
       `Your fortune is: ${fortune}`,
     ])
     .catch((error) => `There was an error: ${error}`);
-
-  return result;
 }
 
 function fullSession(question) {
+  //Empty accumulator
   let result = [];
   return welcome().then((message) => {
+    //push welcome's message
     result.push(message);
+    //now we need to getFortune
     return getFortune(question).then((returnValue) => {
+      //we use concat to avoid embedding an array
       result = result.concat(returnValue);
+      //now we need goodbye
       return goodbye().then((message) => {
+        //push goodbye's message
         result.push(message);
+        //russian doll return result which will be returned all the way to the top
         return result;
       });
     });
