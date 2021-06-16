0340e934d044f64086f1f22a71b395df89661465
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-06-16 14:43:44 -0500

    starting over with commented code blocks

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-function getFortune(question) {}
+function getFortune(question) {
+  /*return tell(question)
+    .then((fortune) => [
+      `Your question was: ${question}`,
+      `Your fortune is: ${fortune}`,
+    ])
+    .catch((error) => `There was an error: ${error}`);
+    */
+}
 
-function fullSession(question) {}
+function fullSession(question) {
+  /*
+  const result = [];
+  welcome().then((message) => {
+    return result.push(message);
+  });
+  getFortune(question).then((returnValue) => {
+    result.concat(returnValue);
+  });
+  goodbye().then((message) => {
+    console.log(result);
+    return result.push(message);
+  });
+  return result;
+  /**/
+}
 
 module.exports = { getFortune, fullSession };
